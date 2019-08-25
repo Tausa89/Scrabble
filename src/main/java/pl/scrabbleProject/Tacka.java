d17a8b83344ca3c8f794a28052e5024abaa62a5a
Lukasz <kuchtalukasz89@gmail.com>
2019-08-26 00:01:35 +0200

    Added method which create list of players 7 letters.

--- src/main/java/pl/scrabbleProject/Tacka.java
+++ src/main/java/pl/scrabbleProject/Tacka.java
@@ -1,4 +1,14 @@
 package pl.scrabbleProject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tacka {
+
+    private List<Letters> playerListOfLetters = new ArrayList<>();
+
+    public List<Letters> creatingPlayerList(ListOfLetters list){
+
+        return this.playerListOfLetters = HelperMethods.listOfSevenPlayerLetters(list);
+    }
 }
