8f31be66f9f315884f0331b0a6669d88cbd2cbc6
Lukasz <kuchtalukasz89@gmail.com>
2019-08-26 00:38:45 +0200

    Added new methods to HelperMethod class

--- src/main/java/pl/scrabbleProject/HelperMethods.java
+++ src/main/java/pl/scrabbleProject/HelperMethods.java
@@ -11,11 +11,35 @@ public class HelperMethods {
 
         List<Letters> playerList = new ArrayList<>();
         for(int i = 0; i < 7; i++) {
-            Collections.shuffle(lettersList.getLettersList());
-            playerList.add(lettersList.getLettersList().get(0));
-            lettersList.getLettersList().remove(0);
+            shufflingBagOfLettersAndAddingLetterToPlayerList(playerList, lettersList);
+            removingLetterFromBagOfLetters(lettersList);
         }
         return playerList;
+    }
+
+    public static void drawingNewLetter(List<Letters> playerListOfLetters, ListOfLetters lettersList){
+        for (int i = 0; i < 7 - playerListOfLetters.size(); i++){
+            shufflingBagOfLettersAndAddingLetterToPlayerList(playerListOfLetters, lettersList);
+            removingLetterFromBagOfLetters(lettersList);
+
+        }
+    }
+
+    public static void shufflingBagOfLettersAndAddingLetterToPlayerList(List<Letters> playerListOfLetters, ListOfLetters lettersList) {
+        Collections.shuffle(lettersList.getLettersList());
+        playerListOfLetters.add(lettersList.getLettersList().get(0));
+    }
+
+    public static void removingLetterFromBagOfLetters(ListOfLetters lettersList) {
+        lettersList.getLettersList().remove(0);
+    }
+
+    public static void removingLetterFromPlayerListOfLetter(List<Letters> playerListOfLetters, Letters letter){
+        if(!playerListOfLetters.isEmpty()){
+            playerListOfLetters.remove(letter);
+        }else
+            System.out.println("No more Letters");
+
     }
 
 
