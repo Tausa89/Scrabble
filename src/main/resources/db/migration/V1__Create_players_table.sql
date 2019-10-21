2b8ec410549632ef0844afe2fb7c2671167bf18f
Lukasz <kuchtalukasz89@gmail.com>
2019-10-22 01:30:45 +0200

    added Table in h2 db

---
+++ src/main/resources/db/migration/V1__Create_players_table.sql
@@ -0,0 +1,8 @@
+create table playersData (
+    ID int unsigned primary key auto_increment,
+    name varchar(25) not null,
+    numberOfWins varchar(3),
+    numberOfDr varchar(3),
+    numberOfWins varchar(3),
+
+);
